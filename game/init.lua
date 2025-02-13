8a24795b096db2ed9bf4b2e8bcb789159988b108
rockgong <rockgong1987@sina.com>
2025-02-13 22:30:38 +0800

    add state saving function

--- game/init.lua
+++ game/init.lua
@@ -17,31 +17,30 @@ game.state = {
     d = false
 }
 
-function game.step(state)
-    if state.d then
-        state.x = state.x + 1
-        state.y = state.y + 1
-    end
-end
-function game.draw(state)
+function game:draw()
+    love.graphics.clear(0.2, 0.2, 0.4, 1.0)
     love.graphics.setFont(ENGINE.assets.get("normal_font"))
-    love.graphics.print(lume.serialize(state), 10, 10)
+    love.graphics.print(lume.serialize(self.state), 10, 10)
     love.graphics.print("Hi, I'm Mark Brown, and this is Game Maker's Toolkit", 10, 30)
     love.graphics.setFont(ENGINE.assets.get("big_font"))
-    love.graphics.print(lume.serialize(state), 10, 100)
-    love.graphics.print("Hi, I'm Mark Brown, and this is Game Maker's Toolkit", 10, 150)
-    love.graphics.draw(ENGINE.assets.get("my_icon"), state.x, state.y)
+    love.graphics.print(lume.serialize(self.state), 10, 100)
+    love.graphics.print("Hi, I'm Mark Brown, and this is Game Maker's Toolkit...", 10, 150)
+    love.graphics.draw(ENGINE.assets.get("my_icon"), self.state.x, self.state.y)
 end
 
-function game.command(state, cmd)
-    print(lume.serialize(cmd))
-    if cmd.type == "keypressed" then
+function game:command(cmd, events)
+    if not cmd.type then
+        if self.state.d then
+            self.state.x = self.state.x + 1
+            self.state.y = self.state.y + 1
+        end
+    elseif cmd.type == "keypressed" then
         if cmd.key == "d" then
-            state.d = true
+            self.state.d = true
         end
     elseif cmd.type == "keyreleased" then
         if cmd.key == "d" then
-            state.d = false
+            self.state.d = false
         end
     end
 end
--- main.lua
+++ main.lua
@@ -1,8 +1,14 @@
 lume = require "lib.lume"
 ENGINE = require"engine"
 
+local game
 local lurker = require("test.lib.lurker")
 local bins = {}
+local state_slot_idx = 0
+local bottom_toast_state = {
+    content = nil,
+    start_time = -1
+}
 
 function love.load()
     bins = lurker.listdir("test/bin")
@@ -10,14 +16,36 @@ end
 
 function love.update(dt)
     lurker.update()
-    if GAME then
-        GAME.step(GAME.state)
+    if game then
+        game:command({})
     end
 end
 
+local function _print_bottom_toast()
+    if bottom_toast_state.content then
+        local advance_time = love.timer.getTime() - bottom_toast_state.start_time 
+        if advance_time < 4 then
+            local r, g, b, a = love.graphics.getColor()
+            local seX, seY, seW, seH = love.window.getSafeArea()
+            local current_alpha = lume.clamp((4 - advance_time) * 3, 0, 1)
+            love.graphics.setColor(r * 0.1, g * 0.1, b * 0.1, current_alpha * 0.5)
+            love.graphics.rectangle("fill", 0, seH - 32, 400, 20)
+            love.graphics.setColor(r, g, b, current_alpha)
+            love.graphics.print(bottom_toast_state.content, 10, seH - 30)
+            love.graphics.setColor(r, g, b, a)
+        end
+    end
+end
+
+local function _toast(str)
+    bottom_toast_state.content = str
+    bottom_toast_state.start_time = love.timer.getTime()
+end
+
 function love.draw()
-    if GAME then
-        GAME.draw(GAME.state)
+    local current_font = love.graphics.getFont()
+    if game then
+        game:draw()
     else
         local y = 10
         love.graphics.print("No Game", 10, y)
@@ -27,24 +55,44 @@ function love.draw()
             y = y + 20
         end
     end
+    love.graphics.setFont(current_font)
+    _print_bottom_toast()
+end
+
+local function _slot_file_name()
+    return string.format("state_slot_%d.txt", state_slot_idx)
 end
 
 function love.keypressed(k)
-    if GAME then
-        GAME.command(GAME.state, {
-            type = "keypressed",
-            key = k
-        })
+    if game then
+        if love.keyboard.isDown("lctrl") then
+            local num = tonumber(k)
+            if num then
+                state_slot_idx = num
+                _toast(string.format("switch to slot: #%s", k))
+            elseif k == "q" then
+                love.filesystem.write(_slot_file_name(), lume.serialize(game.state))
+                _toast(string.format("state saved: #%d", state_slot_idx))
+            elseif k == "e" then
+                game.state = lume.deserialize(love.filesystem.read(_slot_file_name()))
+                _toast(string.format("state loaded: #%d", state_slot_idx))
+            end
+        else
+            game:command({
+                type = "keypressed",
+                key = k
+            })
+        end
     else
         if k == "space" then
-            GAME = require("game")
-            ENGINE.assets.setup(GAME.asset_data)
+            game = require("game")
+            ENGINE.assets.setup(game.asset_data)
         else
             local game_idx = tonumber(k)
             if game_idx then
                 local game_path = bins[game_idx]
                 local result, err = pcall(function()
-                    GAME = require(game_path)
+                    game = require(game_path)
                 end)
                 if not result then
                     print(err)
@@ -55,8 +103,8 @@ function love.keypressed(k)
 end
 
 function love.keyreleased(k)
-    if GAME then
-        GAME.command(GAME.state, {
+    if game then
+        game:command({
             type = "keyreleased",
             key = k
         })
